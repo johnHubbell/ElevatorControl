298241e140f9b45cc1425a888b7c6e1790a961ac
j.c.hubbell0713@gmail.com <j.c.hubbell0713@gmail.com>
2023-07-12 06:05:21 -0500

    feat(test): complete elevator unit test

--- ElevatorControl/ElevatorControl.Tests/Service/ElevatorTests.cs
+++ ElevatorControl/ElevatorControl.Tests/Service/ElevatorTests.cs
@@ -1,10 +1,19 @@
-﻿using ElevatorControl.Service.Services;
+﻿using ElevatorControl.Service.Interfaces;
+using ElevatorControl.Service.Services;
+using FakeItEasy;
 using FluentAssertions;
 
 namespace ElevatorControl.Tests.Service
 {
     public class ElevatorTests
     {
+        private readonly IElevator _elevator;
+
+        public ElevatorTests()
+        {
+            _elevator = A.Fake<IElevator>();
+        }
+
         [Fact]
         public void Elevator_AddNewDestination_TotalRequest()
         {
@@ -18,5 +27,69 @@ namespace ElevatorControl.Tests.Service
             result.Should().BeGreaterThan(0);
             result.Should().Be(1);
         }
+
+        [Fact]
+        public void Elevator_GetCurrentFloor_ReturnInt()
+        {
+            //Arrange
+            var elevator = new Elevator();
+
+            //Act
+            var result = elevator.GetCurrentFloor();
+
+            //Assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Elevator_GetDirection_ReturnNone()
+        {
+            //Arrange
+            var elevator = new Elevator();
+
+            //Act
+            var result = elevator.GetDirection();
+
+            //Assert
+            result.Should().Be(Model.Enums.ElevatorDirection.Elevator_None);
+        }
+
+        [Fact]
+        public void Elevator_GetTotalRequests_ReturnInt()
+        {
+            //Arrange
+            var elevator = new Elevator();
+
+            //Act
+            elevator.AddNewDestination(1);
+            elevator.AddNewDestination(2);
+            elevator.AddNewDestination(3);
+            var result = elevator.GetTotalRequests();
+
+            //Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Elevator_MoveAndCheckIfServed_ReturnFloorNumber()
+        {
+            //Arrange
+            var elevator = new Elevator();
+
+            //Act
+            elevator.AddNewDestination(1);
+            elevator.AddNewDestination(2);
+            elevator.AddNewDestination(3);
+            var result1 = elevator.MoveAndCheckIfServed();
+            var result2 = elevator.GetCurrentFloor();
+            var result3 = elevator.GetNextDestionationFloor();
+            
+
+            //Assert
+            result1.Should().Be(false);
+            result2.Should().Be(1);
+            result3.Should().Be(1);
+        }
+
     }
 }
