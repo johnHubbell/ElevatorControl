e3acdc7e717c46cb32fa35a13739f8cc856d50b1
j.c.hubbell0713@gmail.com <j.c.hubbell0713@gmail.com>
2023-07-12 05:06:41 -0500

    feat(test): Add test project to solution

---
+++ ElevatorControl/ElevatorControl.Tests/Service/ElevatorTests.cs
@@ -0,0 +1,22 @@
+﻿using ElevatorControl.Service.Services;
+using FluentAssertions;
+
+namespace ElevatorControl.Tests.Service
+{
+    public class ElevatorTests
+    {
+        [Fact]
+        public void Elevator_AddNewDestination_TotalRequest()
+        {
+            //Arrange
+            var elevator = new Elevator();
+
+            //Act
+            elevator.AddNewDestination(1);
+            var result = elevator.GetTotalRequests();
+            //Assert
+            result.Should().BeGreaterThan(0);
+            result.Should().Be(1);
+        }
+    }
+}
---
+++ ElevatorControl/ElevatorControl.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ElevatorControl.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
