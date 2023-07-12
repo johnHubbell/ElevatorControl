253f1a19ee7065472f44279f87492d67bf2dc870
j.c.hubbell0713@gmail.com <j.c.hubbell0713@gmail.com>
2023-07-11 19:01:14 -0500

    feat(service): add elevator service

---
+++ ElevatorControl.Api/ElevatorControl.Model/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace ElevatorControl.Model
+{
+    public class Enums
+    {
+        public enum ElevatorDirection
+        {
+            /// <summary>
+            /// Elevator is moving UP.
+            /// </summary>
+            Elevator_Up = 1,
+
+            /// <summary>
+            /// Elevator is moving DOWN.
+            /// </summary>
+            Elevator_Down = 2,
+
+            /// <summary>
+            /// Elevator was in NONE state which means elevator hasn't serviced any request.
+            /// </summary>
+            Elevator_None = 3
+        }
+    }
+}
